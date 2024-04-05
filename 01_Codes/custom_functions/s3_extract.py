06d2c68bcd9c6dd98af7f853f2b60a1934085b18
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2024-04-05 13:59:51 +0000

    s3 extract

--- 01_Codes/custom_functions/s3_extract.py
+++ 01_Codes/custom_functions/s3_extract.py
@@ -12,21 +12,42 @@ def extract_s3_zip(file_name, temp=True):
     """
     # saving the current directory
     current_dir = os.getcwd()
-    print("Initial Directory: ", current_dir)
 
     # changing the directory to the root directory for easier extraction
-    os.chdir("/")
-    print("Current Directory: ", os.getcwd())
+    os.chdir("/workspaces/Diabetic-Retinopathy")
+
+    if not os.path.exists("aws_s3"):
+        print(
+            f"The aws_s3 directory does not exist, please download the data first using the s3_pull.sh script"
+        )
+        return None
+
+    if ".zip" in file_name:
+        file_name = file_name.split(".zip")[0]
+
+    # creating the directory to extract the files
+    if temp:
+        dest_path = "aws_s3"
+    else:
+        # os.mkdir(file_name)
+        dest_path = "./"
+
+    source_path = "aws_s3/" + file_name + ".zip"
+
+    with zipfile.ZipFile(source_path, "r") as zip_ref:
+        zip_ref.extractall(dest_path)
+    print(f"Extracted the files to {dest_path}")
+
+    # Deleting the __MACOSX folder if it exists
+    if os.path.exists(dest_path + "/__MACOSX"):
+        os.system(f"rm -r {dest_path}/__MACOSX")
+
+    os.chdir(current_dir)
     return None
-    # if os.path.exists('aws_s3'):
-    # if temp:
-    #     dest_path =
-    # with zipfile.ZipFile(file_name+".zip", 'r') as zip_ref:
-    #     zip_ref.extractall()
-    # if temp:
-    #     os.remove(file_name)
-    # return None
 
 
 if __name__ == "__main__":
-    extract_s3_zip("aws_s3")
+    # list of all zip files in the aws_s3 directory
+    for i in os.listdir("aws_s3"):
+        if ".zip" in i:
+            extract_s3_zip(i, True)
