038078186a14834254a6936c38259434d717a8a5
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2024-04-05 13:15:41 +0000

    updated

---
+++ s3_pull.sh
@@ -0,0 +1,2 @@
+## Pulling Data from S3
+aws s3 --no-sign-request sync s3://ml-diabetic-retinopathy aws_s3 
--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@ sudo apt-get update
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-rm -rf awscliv2.zip aws
+
+## Deleting the setup files
+rm -rf awscliv2.zip 
+rm -rf aws
