665f48ea0bfa77bd07a5bb4d5fbd15104e1607c7
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2024-04-10 03:17:07 +0000

    update

--- 01_Codes/custom_functions/gpu_check.py
+++ 01_Codes/custom_functions/gpu_check.py
@@ -12,11 +12,22 @@ def gpu_check():
     try:
         subprocess.check_output("nvidia-smi")
         print("Nvidia GPU detected!")
+        return 1
     except (
         Exception
     ):  # this command not being found can raise quite a few different errors depending on the configuration
         print("No Nvidia GPU in system!")
+        return 0
 
-
+def tf_version():
+    """
+    Check the version of Tensorflow
+    """
+    import tensorflow as tf
+    print(tf.version.VERSION)
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 if __name__ == "__main__":
-    gpu_check()
+    if gpu_check():
+        tf_version()
+    pass
+        
