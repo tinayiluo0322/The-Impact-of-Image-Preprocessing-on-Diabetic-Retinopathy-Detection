038a8b318587f248f59451ce71ed5d43ac69393b
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2024-04-14 11:35:37 +0000

    updated startup apps

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+
 ## Install AWS CLI
 sudo apt-get update
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -7,3 +8,9 @@ sudo ./aws/install
 ## Deleting the setup files
 rm -rf awscliv2.zip 
 rm -rf aws
+
+## Installing Tmux
+sudo apt-get -y install tmux
+
+## Installing glances
+pip install --user glances
